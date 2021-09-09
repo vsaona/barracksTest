7d72bf788f91969a215332645bab4bb401ecf403
Vicente Saona <vicente.saona@sansano.usm.cl>
2021-09-09 19:48:18 -0300

    Ready NASA data usage

--- seccion2/src/app/data/data.component.ts
+++ seccion2/src/app/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 
@@ -10,16 +10,88 @@ import { Router } from '@angular/router';
 })
 export class DataComponent implements OnInit {
 
-  constructor(private router: Router, private cookies: CookieService) { }
+  constructor(private http: HttpClient, private router: Router, private cookies: CookieService) { }
 
   token: string = this.cookies.get("token");
-  nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=S2RnsPCdMy60xccgdbtLmv256OSesj9YcwG3bVol&date=2021-09-02";
+  nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=S2RnsPCdMy60xccgdbtLmv256OSesj9YcwG3bVol&date=";
+
+  imgs:Array<Object> = [];
+  imgSources:Array<String> = [];
+  imgTitles:Array<String> = [];
+  imgExplanations:Array<String> = [];
+  imgCopyrights:Array<String> = [];
+
+  pokemonList: Array<Object> = [];
+
+  slideIndex = 1;
 
   ngOnInit(): void {
     this.token = this.cookies.get("token");
     if(!this.token) {
       this.router.navigate(['/account/login']);
     }
+    
+    
+    var date = new Date();
+    console.log("imgs");
+    console.log(this.imgs);
+    for(var i = 1; i <= 3; i++) {
+      this.http.get<any>(this.nasaUrl + date.toISOString().split('T')[0]).subscribe(data => {
+        console.log(data);
+        this.imgs.push(data);
+        this.imgSources.push(data["url"]);
+        this.imgTitles.push(data["title"] + " / " + data["date"]);
+        this.imgExplanations.push(data["explanation"].substring(0,50));
+        this.imgCopyrights.push(data["copyright"]);
+        this.showSlides(1);
+      });
+      date.setDate(date.getDate() - 1);
+    }
+    console.log("imgs");
+    console.log(this.imgs);
+    
+  }
+
+  activateTab(tabName:string) {
+    var tabIndicators = document.getElementsByClassName("tab");
+    var tabs = document.getElementsByClassName("tab-content");
+    for(var i = 0; i < tabs.length; i++) {
+      tabIndicators[i].classList.remove("active-tab");
+      tabs[i].classList.add("invisible");
+      if(tabIndicators[i].id == tabName) {
+        tabIndicators[i].classList.add("active-tab");
+      }
+      if(tabs[i].id == tabName + "-content") {
+        tabs[i].classList.remove("invisible");
+      }
+
+    }
+  }
+
+  plusSlides(n:number) {
+    this.showSlides(this.slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  currentSlide(n:number) {
+    this.showSlides(this.slideIndex = n);
+  }
+  
+  showSlides(n:number) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.add("invisible");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+    console.log(slides);
+    slides[this.slideIndex-1].classList.remove("invisible");
+    dots[this.slideIndex-1].classList.add("active");
   }
 
 }
--- seccion2/src/app/login/login.component.ts
+++ seccion2/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { DataService } from '../data.service';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
