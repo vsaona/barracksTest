04deac4fb928b4eb83354f5d8b8d4f31d27d867a
Vicente Saona <vicente.saona@sansano.usm.cl>
2021-09-09 15:52:37 -0300

    login logic

--- seccion2/src/app/data/data.component.ts
+++ seccion2/src/app/data/data.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-data',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private cookies: CookieService) { }
+
+  token: string = this.cookies.get("token");
+  nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=S2RnsPCdMy60xccgdbtLmv256OSesj9YcwG3bVol&date=2021-09-02";
 
   ngOnInit(): void {
+    this.token = this.cookies.get("token");
+    if(!this.token) {
+      this.router.navigate(['/account/login']);
+    }
   }
 
 }
--- seccion2/src/app/login/login.component.ts
+++ seccion2/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router, private cookies: CookieService) { }
+  token = ""
+  loginRequestBody = {
+    "mail": "testdeveloper@barracks.gg",
+    "password": "testdeveloper"
+  };
 
-  login():void {
-    console.log("yeah");
+  login(): void {
+    console.log("hola");
+    this.http.post<any>('https://beta-api.barracks.gg/v2/Auth/Authenticate', {
+      "mail": "testdeveloper@barracks.gg",
+      "password": "testdeveloper"
+    }, {observe: "response"}).subscribe(data => {
+      if(data.status == 200) {
+        console.log(data.body.token);
+        this.cookies.set("token", data.body.token);
+        this.router.navigate(['/account/data']);
+      } else {
+        alert("¡Ha ingresado mal su usuario y contraseña!\n Código de error: " + data.status);
+      }
+    });
   }
+
   ngOnInit(): void {
   }
 
